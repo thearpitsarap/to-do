67675bdb37a04b93127ef4f9d14e9f5c7f9a69ce
siddharthsutar <ssiddharthsutar@gmail.com>
2023-10-31 00:06:27 +0530

    main page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
-import './index.css';
+import "./index.css";
+import TodoFooter from './components/todo_footer';
+import TodoNavbar from "./components/todo_navbar";
 
 function App() {
   return (
     <>
-
+      <TodoNavbar></TodoNavbar>
+      <main className="container mx-auto my-8 flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold mb-4">Get Organized with My Todo App</h2>
+        <p className="mb-4">
+          My Todo App is the perfect tool to help you stay on top of your tasks
+          and get things done. With our easy-to-use interface, you can quickly
+          add new tasks, set due dates, and mark tasks as complete.
+        </p>
+        <p className="mb-4">
+          Whether you&rsquo;re managing a busy work schedule or just trying to keep
+          track of your personal to-do list, My Todo App has everything you need
+          to stay organized and productive.
+        </p>
+        <a
+          href="#"
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+        >
+          Get Started
+        </a>
+      </main>
+      <TodoFooter></TodoFooter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/todo_footer.jsx
+++ frontend/src/components/todo_footer.jsx
@@ -9,4 +9,4 @@ export default function todo_footer() {
             </footer>
         </>
     )
-}
+}                  
