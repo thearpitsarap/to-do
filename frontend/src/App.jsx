a5c7dcb5fca1e3abed8c60686ab80eb0a02b3bba
siddharthsutar <ssiddharthsutar@gmail.com>
2023-10-31 00:10:06 +0530

    added changes in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,12 @@ function App() {
       <TodoNavbar></TodoNavbar>
       <main className="container mx-auto my-8 flex flex-col items-center justify-center">
         <h2 className="text-3xl font-bold mb-4">Get Organized with My Todo App</h2>
-        <p className="mb-4">
+        <p className="mb-5 px-5 py-5">
           My Todo App is the perfect tool to help you stay on top of your tasks
           and get things done. With our easy-to-use interface, you can quickly
           add new tasks, set due dates, and mark tasks as complete.
         </p>
-        <p className="mb-4">
+        <p className="mb-5 px-5 py-5">
           Whether you&rsquo;re managing a busy work schedule or just trying to keep
           track of your personal to-do list, My Todo App has everything you need
           to stay organized and productive.
