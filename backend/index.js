6c1ee9bd3088a2f2804e22b5b429f824e3902bbc
siddharthsutar <ssiddharthsutar@gmail.com>
2023-10-31 08:27:51 +0530

    added a route to see user

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const { TaskModel1 } = require("./src/utils/models/schema")
 const { userModel } = require("./src/utils/models/userSchema")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-const {validateToken} = require('./src/validateToken')
+const { validateToken } = require('./src/validateToken')
 
 let app = express();
 app.use(express.json())
@@ -15,13 +15,13 @@ app.listen("3000", () => {
 })
 
 app.post("/register", async (req, res) => {
-    const {name,email,password} = req.body;
+    const { name, email, password } = req.body;
     const userExists = await userModel.findOne({ email });
 
     if (userExists) {
         res.status(404).json({ Message: "User Exists" })
     }
-    const hashPassword = await bcrypt.hash(password,10);
+    const hashPassword = await bcrypt.hash(password, 10);
     const user = await userModel.create({
         name,
         email,
@@ -33,45 +33,59 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    const emailExists = await userModel.findOne({email})
-    if(emailExists && (await bcrypt.compare(password,emailExists.password))){
+    const emailExists = await userModel.findOne({ email })
+    if (emailExists && (await bcrypt.compare(password, emailExists.password))) {
         const accessToken = jwt.sign({
-            user:{
-                username:emailExists.name,
-                email:emailExists.email,
-                id:emailExists.id
+            user: {
+                username: emailExists.name,
+                email: emailExists.email,
+                id: emailExists.id
             }
         },
-        access_token_sec,
-        {expiresIn:"15m"});
-        res.status(200).json({accessToken})
+            access_token_sec,
+            { expiresIn: "15m" });
+        res.status(200).json({ accessToken })
     }
-    else{
-        res.status(401).json({message:"Error"})
+    else {
+        res.status(401).json({ message: "Error" })
     }
 })
 
-app.get("/current",validateToken,(req, res) => {
+app.get("/user/:email", async (req, res) => {
+    const email = req.params.email;
+    const user = await userModel.findOne({ email });
+    if (user) {
+        res.status(200).json({
+            username: user.name,
+            email: user.email,
+            id: user.id,
+        });
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
+
+app.get("/current", validateToken, (req, res) => {
     res.json(req.user)
 })
 
 app.get("/", validateToken, async (req, res) => {
-    const contact = await TaskModel1.find({user_id:req.user.id});
+    const contact = await TaskModel1.find({ user_id: req.user.id });
     res.status(200).json(contact)
 })
 
-app.get("/:id",validateToken, async (req, res) => {
+app.get("/:id", validateToken, async (req, res) => {
     const id = req.params.id;
     const contact = await TaskModel1.findById(id);
     res.status(200).json(contact)
 })
 
-app.post("/", validateToken,async (req, res) => {
+app.post("/", validateToken, async (req, res) => {
     try {
         const name = req.body.name;
         const contact = await TaskModel1.create({
             name,
-            user_id:req.user.id
+            user_id: req.user.id
         });
         res.status(201).json(contact)
     } catch (err) {
@@ -79,7 +93,7 @@ app.post("/", validateToken,async (req, res) => {
     }
 })
 
-app.put("/:id",validateToken, async (req, res) => {
+app.put("/:id", validateToken, async (req, res) => {
     const id = req.params.id
     const contact = await TaskModel1.findById(id);
     const update = await TaskModel1.findByIdAndUpdate(
@@ -89,7 +103,7 @@ app.put("/:id",validateToken, async (req, res) => {
     res.status(200).json(contact)
 })
 
-app.delete("/:id",validateToken, async (req, res) => {
+app.delete("/:id", validateToken, async (req, res) => {
     const id = req.params.id
     const contact = await TaskModel1.findOneAndDelete(id);
     res.status(200).json(contact)
