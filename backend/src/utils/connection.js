36cd7fb878a33f0ae2137108ea31aae6a96535b3
siddharthsutar <ssiddharthsutar@gmail.com>
2023-10-31 08:50:58 +0530

    change in connection

--- backend/src/utils/connection.js
+++ backend/src/utils/connection.js
@@ -1,14 +1,21 @@
-const mongoose = require("mongoose"); 
-// Connecting to database 
+const mongoose = require("mongoose");
+
+// Connecting to database
 const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/todoApp`, {
-        useNewUrlParser: true,
-      });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(`mongodb://localhost:27017/`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    const db = conn.connection.useDb("todoApp");
+
+    const todoCollection = db.collection("todos");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
   }
-module.exports = {connectDB};
+};
+
+module.exports = { connectDB };
