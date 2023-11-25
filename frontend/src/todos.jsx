9f7792baf84b6cc76c1892f7833f069aee5a5db8
siddharthsutar <ssiddharthsutar@gmail.com>
2023-11-25 22:34:33 +0530

    updated todo

--- frontend/src/todos.jsx
+++ frontend/src/todos.jsx
@@ -3,15 +3,29 @@ import TodoComponent from './components/todo_component';
 
 const Todos = () => {
     const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
     useEffect(() => {
-        fetchTodos().then(setTodos);
-    }, []);
+        const fetchAndSetTodos = async () => {
+            const fetchedTodos = await fetchTodos();
+            if (JSON.stringify(fetchedTodos) !== JSON.stringify(todos)) {
+                setTodos(fetchedTodos);
+            }
+        };
+        fetchAndSetTodos();
+    }, [todos]);
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
     return (
-        <div>
-            <h1>Todos</h1>
-            {todos.map(todo => <TodoComponent key={todo.id} todo={todo} />)}
-        </div>
+        <>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required onChange={handleInputChange}></input>
+            <div>
+                {todos.map(todo => <TodoComponent key={todo.id} todo={todo} />)}
+            </div>
+        </>
     );
 };
 
