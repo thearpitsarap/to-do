0bd3c18c6e7c0eff1e8e6c69ab5154c38168f198
siddharthsutar <ssiddharthsutar@gmail.com>
2023-11-30 23:01:38 +0530

    corrected padding

--- frontend/src/components/todo_component.jsx
+++ frontend/src/components/todo_component.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const List = ({ todos }) => {
     return (
-        <ul className="p-4 space-y-4 bg-white bg-opacity-25 border border-gray-200 shadow-xl rounded-xl backdrop-filter backdrop-blur-lg">
+        <ul className="p-6 space-y-4 bg-white bg-opacity-25 border border-gray-200 shadow-xl rounded-xl backdrop-filter backdrop-blur-lg">
             {todos.map((todo) => (
                 <li key={todo.id} className="p-2 font-semibold transition-transform duration-200 ease-in-out transform bg-white bg-opacity-25 border border-gray-200 shadow-xl rounded-xl backdrop-filter backdrop-blur-lg hover:scale-105">
                     {todo.title}
